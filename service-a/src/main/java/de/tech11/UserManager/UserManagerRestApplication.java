13c59b1c2e8565dac0c3c5e4faa11b72f3e1ea48
mmeyer <mmeyer@acd-synergie.de>
2020-07-31 13:17:17 +0200

    Initial commit
    Initial commit

---
+++ service-a/src/main/java/de/tech11/UserManager/UserManagerRestApplication.java
@@ -0,0 +1,11 @@
+package de.tech11.UserManager;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class UserManagerRestApplication extends Application {
+}
