86162898d188685672defee8474dcc5e74379796
mmeyer <mmeyer@acd-synergie.de>
2020-08-02 13:25:50 +0200

    'Pojo'

---
+++ backendside/src/main/java/de/tech11/UserManager/models/IUser.java
@@ -0,0 +1,9 @@
+package de.tech11.UserManager.models;
+
+/**
+ * Class representing the entity version of IUser interface
+ */
+public interface IUser
+{
+
+}
---
+++ backendside/src/main/java/de/tech11/UserManager/models/UserEntity.java
@@ -0,0 +1,10 @@
+package de.tech11.UserManager.models;
+
+/**
+ * Class representing the entity version of IUser interface
+ */
+public class UserEntity
+{
+		@Id
+		private long id;
+}
